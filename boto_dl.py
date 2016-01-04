4bb3b5290d8c2c1c2b67a12c89f41f967e6cf915
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-04 13:14:45 -0600

    update boto_dl.py with Mark's suggestions -- you should do the ul script next

--- boto_dl.py
+++ boto_dl.py
@@ -18,18 +18,32 @@ parser.add_argument('-a','--access_key', help='file to download', required=True)
 parser.add_argument('-s','--secret_key', help='file to download', required=True)
 parser.add_argument('-b','--bucket_name', help='file to download', required=True)
 parser.add_argument('-g','--gateway', help='file to download', required=True)
+parser.add_argument('-u','--useparcel', help='file to download', required=True)
+#parser.add_argument('-p','--port', help='only used with -u|--useparcel, port number for parcel', default="9000")
+
 #parser.add_argument('-b','--bar', help='Description for bar argument', required=True)
 #args = vars(parser.parse_args())
 args = parser.parse_args()
 
 ### Open connection
-conn = boto.connect_s3(
-        aws_access_key_id = args.access_key,
-        aws_secret_access_key = args.secret_key,
-        host = args.gateway,
-        #is_secure=False, # uncomment if you are not using ssl
-        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
-        )
+if args.useparcel == "useparcel":
+    conn = boto.connect_s3(
+    aws_access_key_id = args.access_key,
+    aws_secret_access_key = args.secret_key,
+    host = args.gateway,
+    #port = 9000
+    #is_secure=False, # uncomment if you are not using ssl
+    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    )
+else:
+    conn = boto.connect_s3(
+    aws_access_key_id = args.access_key,
+    aws_secret_access_key = args.secret_key,
+    host = args.gateway,
+    port = 9000,
+    #is_secure=False, # uncomment if you are not using ssl
+    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    )
 
 ### get existing bucke | create newt::
 bucket = conn.get_bucket(args.bucket_name)
@@ -37,4 +51,11 @@ bucket = conn.get_bucket(args.bucket_name)
 ### downloading an object to local::
 key = bucket.get_key(args.file)
 key.get_contents_to_filename('./' + args.file)
+
+# key = conn.get_bucket(args.bucket).get_key(args.key)
+#     if key is None:
+#         key = conn.get_bucket(args.bucket).new_key(args.key)
+
+#     key.set_contents_from_file(sys.stdin)
+
 
