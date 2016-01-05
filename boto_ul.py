4aea11e3234b60d38bbb71643c2ed2202a6a72d5
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-05 16:27:46 -0600

    updated boto_ul.py

--- boto_ul.py
+++ boto_ul.py
@@ -12,26 +12,37 @@ import boto.s3.connection
 
 print("example file: \"ERR_tar.12Mb.gz\"")
 
-parser = argparse.ArgumentParser(description='Simple script to perform a boto upload')
-parser.add_argument('-f','--file', help='file to upload', required=True)
+parser = argparse.ArgumentParser(description='Simple script to perform a boto download')
+parser.add_argument('-f','--file', help='file to download', required=True, default="test")
 parser.add_argument('-a','--access_key', help='file to download', required=True)
 parser.add_argument('-s','--secret_key', help='file to download', required=True)
 parser.add_argument('-b','--bucket_name', help='file to download', required=True)
 parser.add_argument('-g','--gateway', help='file to download', required=True)
-#parser.add_argument('-b','--bar', help='Description for bar argument', required=True)
-#args = vars(parser.parse_args())
+parser.add_argument('-p','--useparcel', help='file to download', required=False, action='store_true', default=False)
 args = parser.parse_args()
 
 ### Open connection
-conn = boto.connect_s3(
-        aws_access_key_id = args.access_key,
-        aws_secret_access_key = args.secret_key,
-        host = args.gateway,
-        #is_secure=False, # uncomment if you are not using ssl
-        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
-        )
+if args.useparcel:
+    print "\n\nBoto download Using Parcel\n\n"
+    conn = boto.connect_s3(
+    aws_access_key_id = args.access_key,
+    aws_secret_access_key = args.secret_key,
+    host = args.gateway,
+    port = 9000,
+    #is_secure=False, # uncomment if you are not using ssl
+    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    )
+else:
+    print "\n\nBoto download Without Parcel\n\n"
+    conn = boto.connect_s3(
+    aws_access_key_id = args.access_key,
+    aws_secret_access_key = args.secret_key,
+    host = args.gateway,
+    #is_secure=False, # uncomment if you are not using ssl
+    calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+    )
 
-### get existing bucke | create newt::
+### get existing bucket | create new::
 bucket = conn.get_bucket(args.bucket_name)
 
 ### Upload object to bucket::
