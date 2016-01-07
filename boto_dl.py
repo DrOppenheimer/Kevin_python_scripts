6f5a27958bf93ff8afc9a2cec70c6fe06f178c71
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-01-07 11:13:47 -0600

    update/debug

--- boto_dl.py
+++ boto_dl.py
@@ -14,11 +14,11 @@ import boto.s3.connection
 
 parser = argparse.ArgumentParser(description='Simple script to perform a boto download')
 parser.add_argument('-f','--file', help='file to download', required=True, default="test")
-parser.add_argument('-a','--access_key', help='file to download', required=True)
-parser.add_argument('-s','--secret_key', help='file to download', required=True)
-parser.add_argument('-b','--bucket_name', help='file to download', required=True)
-parser.add_argument('-g','--gateway', help='file to download', required=True)
-parser.add_argument('-p','--useparcel', help='file to download', required=False, action='store_true', default=False)
+parser.add_argument('-a','--access_key', help='access key', required=True)
+parser.add_argument('-s','--secret_key', help='secret key', required=True)
+parser.add_argument('-b','--bucket_name', help='bucket name', required=True)
+parser.add_argument('-g','--gateway', help='gateway', required=True)
+parser.add_argument('-p','--useparcel', help='option to use parcel', required=False, action='store_true', default=False)
 
 #parser.add_argument('-b','--bar', help='Description for bar argument', required=True)
 #args = vars(parser.parse_args())
@@ -31,7 +31,7 @@ if args.useparcel:
     aws_access_key_id = args.access_key,
     aws_secret_access_key = args.secret_key,
     host = args.gateway,
-    #port = 9000,
+    #port = 9000, # I don;t get this at all, script crashes when this is uncommented and run with test_rw_beta.sh, but it runs fine when executed by itself
     #is_secure=False, # uncomment if you are not using ssl
     calling_format = boto.s3.connection.OrdinaryCallingFormat(),
     )
