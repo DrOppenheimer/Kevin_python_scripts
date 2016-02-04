8a537f22eef12a3bd3b4e702d76050bbb37f6a85
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-02-04 10:53:01 -0600

    update generate_file_md5.py with shebang

--- generate_file_md5.py
+++ generate_file_md5.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+
 # md5 large files
 # http://stackoverflow.com/questions/1131220/get-md5-hash-of-big-files-in-python
 # execfile("~/git/Kevin_python_scripts/generate_file_md5.py")
+
 def generate_file_md5(filename, blocksize=2**20):
     m = hashlib.md5()
     with open( os.path.join(filename) , "rb" ) as f:
