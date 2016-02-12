a3fb33e46f7d8d27ee28bce8adaaf0a0380f5f7d
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-02-12 14:40:40 -0600

    debug generate_md5...

--- generate_file_md5.py
+++ generate_file_md5.py
@@ -6,7 +6,7 @@
 
 import hashlib
 
-def checksum_md5(filename):
+def generate_file_md5(filename):
     md5 = hashlib.md5()
     with open(filename,'rb') as f: 
         for chunk in iter(lambda: f.read(8192), b''): 
