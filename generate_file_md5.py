66706c911f85799cb0ae54f7f3180d3243875e05
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-02-12 14:29:25 -0600

    debug generate_md5...

--- generate_file_md5.py
+++ generate_file_md5.py
@@ -4,15 +4,27 @@
 # http://stackoverflow.com/questions/1131220/get-md5-hash-of-big-files-in-python
 # execfile("~/git/Kevin_python_scripts/generate_file_md5.py")
 
-import hashlib
+#import hashlib
 import os
+import subprocess
 
-def generate_file_md5(rootdir, filename, blocksize=2**20):
+#def generate_file_md5(rootdir, filename, blocksize=2**20):
+def generate_file_md5(filename, blocksize=2**20):
     m = hashlib.md5()
-    with open( os.path.join(rootdir, filename) , "rb" ) as f:
+    #with open( os.path.join(rootdir, filename) , "rb" ) as f:
+    with open( filename , "rb" ) as f:
         while True:
             buf = f.read(blocksize)
             if not buf:
                 break
             m.update( buf )
             return m.hexdigest()
+
+
+# def generate_file_md5(file):
+#     os_string = "md5sum " + file + " | cut -d \" \" -f 1"
+#     print os_string
+#     my_md5 = subprocess.check_output(os_string, shell=True)
+#     return my_md5
+
+#result = subprocess.check_output("md5sum HG00101.alt_bwamem_GRCh38DH.20150826.GBR.exome.cram | cut -d \" \" -f 1", shell=True)
