67ee3330d6209ee563079ade35a5134a87f00131
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-02-12 10:35:34 -0600

    remove *.pyc files

--- ftp_2_s3.beta.py
+++ ftp_2_s3.beta.py
@@ -18,9 +18,18 @@ parser.add_argument('-s','--secret_key', help='secret key', required=True)
 parser.add_argument('-b','--bucket_name', help='bucket name', default="1000_genome_exome")
 parser.add_argument('-c','--credentials', help='credentials file for multipart upload: access_key, secret_key', required=True)
 parser.add_argument('-r', '--retry', help='number of times to retry each download', default=10)
+parser.add_argument('-k', '--compare_md5', help='provide a list ( name \t md5 ) to compare against', default=0)
 args = parser.parse_args()
 
-def ftp_dl(line, access_key, secret_key, bucket_name):
+def get_value(my_key, my_dictionary):
+    if my_dictionary.has_key(my_key):
+        my_value = my_dictionary.get(my_key)
+        my_value = my_value.rstrip("\n")
+        return (my_value)
+    else:        
+        return ("key does not exist")
+
+def ftp_dl(line, fileName, access_key, secret_key, bucket_name, compare_md5, md5_ref_dictionary):
     line = line.rstrip("\n")
     tic = time.time()
     wget_status=subprocess.call(["wget", line])
@@ -28,6 +37,15 @@ def ftp_dl(line, access_key, secret_key, bucket_name):
     if wget_status == 0:
         print ("calculating dl md5 " + fileName)   #### get md5 for file downloaded from ftp
         dlFileMd5 = generate_file_md5(fileName)    # uses function generate_file_md5 -- in your scripts
+        if compare_md5 != 0:                       ### Option to check against reference md5
+            ref_md5 = get_value(my_key=fileName, my_dictionary=md5_ref_dictionary)
+            if dlFileMd5 == ref_md5:
+                dl_md5_check = "md5_PASS"
+            else:
+                dl_md5_check = "md5_FAIL"
+        else:
+            ref_md5 = "NA"
+            dl_md5_check = "NA"
         statinfo = os.stat(fileName)               #### get size of file downloaded from ftp
         dlSize = statinfo.st_size                  #size_gb = float(size) / (2**30)
         print ("uploading " + fileName)            #### upload to s3
@@ -48,12 +66,21 @@ def ftp_dl(line, access_key, secret_key, bucket_name):
             LOGFILE.write(log_string)
             LOGFILE.flush()
         s3FileMd5=bucket.get_key(key).etag[1 :-1]  ### Get the md5 for the file on s3
+        if compare_md5 != 0:                       ### Option to check against reference md5
+            ref_md5 = get_value(my_key=fileName, my_dictionary=md5_ref_dictionary)
+            if dlFileMd5 == ref_md5:
+                ul_md5_check = "md5_PASS"
+            else:
+                ul_md5_check = "md5_FAIL"
+        else:
+            ref_md5 = "NA"
+            ul_md5_check = "NA"
         s3Size=bucket.lookup(fileName).size                            ### Get the size for the file on s3
         print ("printing to log " + fileName)      #### print to log
         if dlSize > 4*(2**30):
-            log_string = fileName + '\t' + str(dlSize) + '\t' + str(dlFileMd5) + '\t' + str(dlTime) + '\t' + str(s3Size) + '\t' + str(s3FileMd5) + '\t' + str(ulTime) + '\t' + "File > 4Gb (4*(2^30) bytes), used multipart upload - upload md5 WILL NOT match dl md5" '\n'
+            log_string = fileName + '\t' + ref_md5 + '\t' + str(dlSize) + '\t' + str(dlFileMd5) + '\t' + dl_md5_check + '\t' + str(dlTime) + '\t' + str(s3Size) + '\t' + str(s3FileMd5) + '\t' + ul_md5_check + '\t' + str(ulTime) + '\t' + "File > 4Gb (4*(2^30) bytes), used multipart upload - upload md5 WILL NOT match dl md5" '\n'
         else:
-            log_string = fileName + '\t' + str(dlSize) + '\t' + str(dlFileMd5) + '\t' + str(dlTime) + '\t' + str(s3Size) + '\t' + str(s3FileMd5) + '\t' + str(ulTime) + '\n'
+            log_string = fileName + '\t' + ref_md5 + '\t' + str(dlSize) + '\t' + str(dlFileMd5) + '\t' + dl_md5_check + '\t' + str(dlTime) + '\t' + str(s3Size) + '\t' + str(s3FileMd5) + '\t' + ul_md5_check + '\t' + str(ulTime) + '\n'
         print ("Done processing sample ( " + str(sample) + " ) :: " + fileName)
         LOGFILE.write(log_string)
         LOGFILE.flush()
@@ -69,8 +96,18 @@ def ftp_dl(line, access_key, secret_key, bucket_name):
             return wget_status
 
 ### MAIN ###
+
+# read in compare list if option is specified
+if args.compare_md5 != 0:
+    md5_ref_dictionary = {}
+    with open(args.compare_md5) as f:
+        for line in f:
+            (key, val) = line.split('\t')
+            md5_ref_dictionary[key] = val
+
+# heavy lifting 
 LOGFILE = open('./' + args.list + '.ul_log.txt', 'w+')
-LOGFILE.write('file_name' + '\t' + 'local_size(bytes)' + '\t' + 'local_md5' + '\t' + 'dl_time(s)' + '\t' + 's3_size(bytes)' + '\t' + 's3_md5' + '\t' + 'ul_time(s)' + '\n')
+LOGFILE.write('file_name' + '\t' + 'ref_md5' +'\t' + 'local_size(bytes)' + '\t' + 'local_md5' + '\t' + 'local_md5_check' + '\t' + 'dl_time(s)' + '\t' + 's3_size(bytes)' + '\t' + 's3_md5' + '\t' + 's3_md5_check' + '\t' + 'ul_time(s)' + '\n')
 LOGFILE.flush()
 sample=0
 ftp_status=0
@@ -82,9 +119,4 @@ with open(args.list) as f:
         fileName = fileName.rstrip("\n")
         print ("Processing sample ( " + str(sample) + " ) :: " + fileName)
         while ftp_status < args.retry:
-            ftp_status=ftp_dl(line=line, access_key=args.access_key, secret_key=args.secret_key, bucket_name=args.bucket_name)
-        
-    
-    
-                    
-        
+            ftp_status=ftp_dl(line=line, fileName=fileName, access_key=args.access_key, secret_key=args.secret_key, bucket_name=args.bucket_name, compare_md5=compare_md5, md5_ref_dictionary=md5_ref_dictionary)        
